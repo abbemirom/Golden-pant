f2b30a896f1bd481665ee2eee15e8ac9bca9d60d
Lucas Olofsson <lucas.olofsson@abbgymnasiet.se>
2022-09-07 10:42:45 +0200

    NavBar komponent + NavBar i index

---
+++ Golden/components/Buttons.vue
@@ -0,0 +1,17 @@
+v-btn/prop-variant.vue
+<template>
+  <div class="d-flex justify-space-between">
+    <button id="menu-button"><img src="https://cdn.icon-icons.com/icons2/916/PNG/512/Menu_icon_icon-icons.com_71858.png" class="w-10" /></button>
+  </div>
+</template>
+
+<style>
+  #menu-button {
+    background-color: white;
+    border: 2px;
+    border-style: solid;
+    border-color: black;
+    border-radius: 100%;
+    padding: 20px;
+  }
+  </style>
--- Golden/pages/index.vue
+++ Golden/pages/index.vue
@@ -1,12 +1,30 @@
 <template>
   <div>
+    <NavBar></NavBar>
     <!-- <Tutorial/> -->
-    <div>hej</div>
+    <div>hejhej</div>
+    <div>hejhej</div>
+    <div>hejhej</div>
+    <div>hejhej</div>
+    <div>hejhej</div>
+    <div>hejhej</div>
+
+
   </div>
 </template>
 
 <script>
+import Buttons from '/components/Buttons.vue'
+import NavBar from '/components/NavBar.vue'
+
 export default {
-  name: 'IndexPage'
+  name: 'IndexPage',
+
+  layout: 'default',
+
+  components: {
+    Buttons,
+    NavBar
+  }
 }
 </script>
