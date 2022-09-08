f07a4c68617cd38907b43a6359d9b0951faca7b9
Emilia Romberg <emilia.romberg@abbgymnasiet.se>
2022-09-08 17:55:19 +0200

    gott och blandat, index och ett försök till karta

---
+++ Golden/components/BokLista.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="text-left">Böcker</div>
+    <div class="flex flex-row justify-center">
+      <div class="w-1/4 h-20 md:h-40 bg-black my-10 mx-2 text-white rounded">
+        Bild
+      </div>
+      <div class="w-1/4 h-20 md:h-40 bg-black my-10 mx-2 text-white rounded">
+        Bild
+      </div>
+      <div class="w-1/4 h-20 md:h-40 bg-black my-10 mx-2 text-white rounded">
+        Bild
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>>
+
+<style>
+</style>
--- Golden/pages/index.vue
+++ Golden/pages/index.vue
@@ -1,12 +1,28 @@
 <template>
   <div>
-
-    <div>hej</div>
+    <div class="text-xl text-center mt-4 min-h-screen">
+      Böcker
+      <div class="">
+        <BokLista></BokLista>
+        <!-- <Karta></Karta> -->
+      </div>
+    </div>
+    <div class="min-h-screen bg-yellow-600 flex flex-col justify-center">
+      <div>
+        <img
+          src="https://map02.eniro.no/geowebcache/service/tms1.0.0/map2x/13/4502/5779.png?c=2710559445&v=123"
+          class="ml-5"
+        />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import BokLista from '../components/BokLista.vue'
+import Karta from '../components/Karta.vue'
 export default {
+  components: { BokLista, Karta },
   name: 'IndexPage'
 }
 </script>
