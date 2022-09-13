a5b5b35734b3799539e550cc5657052f97790746
abboliott <oliver.ottosson@abbgymnasiet.se>
2022-09-13 12:18:22 +0200

    hej 2

--- Golden/pages/customer-login.vue
+++ Golden/pages/customer-login.vue
@@ -1,13 +1,45 @@
 <template>
-
+  <div>
+  <input type="text" name="username" v-model="input.username" placeholder="Username" />
+  <input type="password" name="password" v-model="input.password" placeholder="Password" />
+  <button type="button" v-on:click="login()">Login</button>
+  </div>
 </template>
 
 <script>
-export default {
+    export default {
+        name: 'Login',
+        async asyncData() {
+        const data = await fetch("http://localhost:5000/customers", {
+        method: "GET"
+        });
+        return {
+        customers: await data.json(),
 
-}
+
+                input: {
+                    username: "",
+                    password: ""
+                }
+            }
+        },
+        methods: {
+            login() {
+                if(this.input.username != "" && this.input.password != "") {
+                    if(this.customers.map(a => a.email).includes(this.input.username) == true && this.customers.map(a => a.password).includes(this.input.password) == true) {
+                        // this.$emit("authenticated", true);
+                        // this.$router.replace({ name: "secure" });
+                        alert("hej")
+                    } else {
+                        console.log("The username and / or password is incorrect");
+                    }
+                } else {
+                    console.log("A username and password must be present");
+                }
+            }
+        }
+    }
 </script>
-
 <style>
 
 </style>
