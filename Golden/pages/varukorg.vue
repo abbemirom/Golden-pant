7d86f6c9fecca7d2e9f54a79ca829a457af38af3
abboliott <oliver.ottosson@abbgymnasiet.se>
2022-09-13 13:48:31 +0200

    rjgdvucyrb

---
+++ Golden/pages/varukorg.vue
@@ -0,0 +1,25 @@
+<template>
+<div>
+  {{ book1 }}
+</div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      books: []
+    }
+  },
+  async fetch () {
+    this.books = await fetch(
+      "http://localhost:5000/customers"
+    ).then(res => res.json())
+  },
+  book1 = books.Find(book=>book.id==varukorg.id)
+}
+</script>
+
+<style>
+
+</style>
