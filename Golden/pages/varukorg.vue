ece1060a38b98f3f716d15fdccf0e7b7cd3d2b74
abboliott <oliver.ottosson@abbgymnasiet.se>
2022-09-16 12:59:43 +0200

    databas o varukorg

--- Golden/pages/varukorg.vue
+++ Golden/pages/varukorg.vue
@@ -2,9 +2,11 @@
 
 <div>
 <div v-for="user of books" :key="user.id">
-  <h1 >{{user.name}} </h1>
-  <div v-for="book of user.varukorg" :key="book">
-    book
+  <h1> {{user.name}} </h1>
+  <div>
+    {{ user.varukorg[0] }}
+    {{ user.varukorg[1] }}
+    {{ user.varukorg[2] }}
   </div>
   <!-- {{ book1 }} -->
 
@@ -30,7 +32,9 @@ async fetch () {
       "http://localhost:5000/customers"
     ).then(res => {
       const data = res.json()
-      this.books = data
+      console.log(data)
+      return data;
+    //  this.books = data
       })
   },
 },
